aa67b5994773e0d5495ccb878f9bac5e7665a81b
Samuel <adejinmisamuel1@gmail.com>
2025-05-19 13:39:23 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'mygray':'#E5E5E5',
-        'myyellow':'#FFB400',
-        'line':'#F0F0F6',
-        'text':'#2B2B2B'
+      colors: {
+        mygray: "#E5E5E5",
+        myyellow: "#FFB400",
+        line: "#F0F0F6",
+        text: "#2B2B2B",
       },
-      backgroundImage:{
-        'profile':"url('./files/profile-small.jpg')",
-        'art':"url('./files/art.jpg')"
+      fontFamily: {
+        gilroy: ["Gilroy Regular", ...fontFamily.sans],
+        gilroyMedium: ["Gilroy Medium", ...fontFamily.sans],
+        outfitRegular: ["Outfit Regular", ...fontFamily.sans],
       },
-      width:{
-        '3.1/10':'31%'
-      }
+      backgroundImage: {
+        profile: "url('./files/profile-small.jpg')",
+        art: "url('./files/art.jpg')",
+      },
+      width: {
+        "3.1/10": "31%",
+      },
     },
   },
   plugins: [],
-}
-
+};
